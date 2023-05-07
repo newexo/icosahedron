4de6e0139b07a25988f3c8f405748b9fa7eb4e0d
Reuben Brasher <rkbrasher@gmail.com>
2023-05-06 23:21:40 -0700

    Raw generated code for stat roller

---
+++ d20_ai/tests/test_stat_roller.py
@@ -0,0 +1,36 @@
+import unittest
+from ability_score_roller import AbilityScoreRoller
+
+class TestAbilityScoreRoller(unittest.TestCase):
+    def test_roll_ability_scores(self):
+        roller = AbilityScoreRoller()
+        scores = roller.roll_ability_scores()
+
+        self.assertEqual(len(scores), 6)
+        for score in scores:
+            self.assertGreaterEqual(score, 3)
+            self.assertLessEqual(score, 18)
+
+if __name__ == '__main__':
+    unittest.main()
+
+class TestAbilityScoreRoller(unittest.TestCase):
+    def test_roll_ability_scores(self):
+        # test standard ability score rolling
+        roller = AbilityScoreRoller()
+        scores = roller.roll_ability_scores()
+
+        self.assertEqual(len(scores), 6)
+        for score in scores:
+            self.assertGreaterEqual(score, 3)
+            self.assertLessEqual(score, 18)
+
+        # test classic ability score rolling with seed=42
+        roller = ClassicAbilityScoreRoller(seed=42)
+        scores = roller.roll_ability_scores()
+
+        expected_scores = [15, 14, 13, 12, 10, 8]
+        self.assertEqual(scores, expected_scores)
+
+if __name__ == '__main__':
+    unittest.main()
