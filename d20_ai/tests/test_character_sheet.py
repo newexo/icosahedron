e4da1d36ba7f9d4a4bb578c2120ef40127c446e9
Reuben Brasher <rkbrasher@gmail.com>
2023-05-21 12:04:23 -0700

    Simplify CharacterSheet test case

--- d20_ai/tests/test_character_sheet.py
+++ d20_ai/tests/test_character_sheet.py
@@ -1,11 +1,13 @@
 import unittest
+import json
+
+from d20_ai.directories import test_data
 from d20_ai.character_sheet import D20CharacterSheet
 
 
 class TestD20CharacterSheet(unittest.TestCase):
-    def test_init(self):
-        # Test valid inputs
-        alice = D20CharacterSheet(
+    def setUp(self):
+        self.instance = D20CharacterSheet(
             name="Alice",
             race="Human",
             char_class="Wizard",
@@ -52,13 +54,19 @@ class TestD20CharacterSheet(unittest.TestCase):
             background="Alice grew up in a small village where she was fascinated by the natural world and the secrets of the arcane. She left her village to study magic at a nearby wizard academy, where she excelled in her studies and gained a reputation as a diligent and creative student. After graduating, she set out on a journey to explore the world and use her magic to help others. Alice is driven by a desire to uncover new knowledge and",
         )
 
-        self.assertEqual(alice.name, "Alice")
-        self.assertEqual(alice.race, "Human")
-        self.assertEqual(alice.char_class, "Wizard")
-        self.assertEqual(alice.level, 3)
-        self.assertEqual(alice.alignment, "Neutral Good")
+        with open(test_data("alice.json")) as f:
+            self.instance_str = f.read()
+
+        self.instance_dict = json.loads(self.instance_str)
+
+    def instance_test(self, instance):
+        self.assertEqual(instance.name, "Alice")
+        self.assertEqual(instance.race, "Human")
+        self.assertEqual(instance.char_class, "Wizard")
+        self.assertEqual(instance.level, 3)
+        self.assertEqual(instance.alignment, "Neutral Good")
         self.assertEqual(
-            alice.ability_scores,
+            instance.ability_scores,
             {
                 "strength": 8,
                 "dexterity": 14,
@@ -69,22 +77,22 @@ class TestD20CharacterSheet(unittest.TestCase):
             },
         )
         self.assertEqual(
-            alice.skills,
+            instance.skills,
             {"arcana": 6, "history": 6, "investigation": 6, "nature": 6, "religion": 6},
         )
-        self.assertEqual(len(alice.feats), 2)
-        self.assertEqual(alice.feats[0]["name"], "Alert")
+        self.assertEqual(len(instance.feats), 2)
+        self.assertEqual(instance.feats[0]["name"], "Alert")
         self.assertEqual(
-            alice.feats[0]["description"], "+5 initiative and cannot be surprised"
+            instance.feats[0]["description"], "+5 initiative and cannot be surprised"
         )
-        self.assertEqual(alice.feats[1]["name"], "Magic Initiate")
+        self.assertEqual(instance.feats[1]["name"], "Magic Initiate")
         self.assertEqual(
-            alice.feats[1]["description"],
+            instance.feats[1]["description"],
             "Can cast two cantrips and one 1st-level spell from a chosen spellcasting class (cleric)",
         )
-        self.assertEqual(len(alice.equipment), 7)
+        self.assertEqual(len(instance.equipment), 7)
         self.assertEqual(
-            alice.equipment,
+            instance.equipment,
             [
                 "Spellbook",
                 "Wand",
@@ -96,233 +104,35 @@ class TestD20CharacterSheet(unittest.TestCase):
             ],
         )
         self.assertEqual(
-            alice.spells_known,
+            instance.spells_known,
             {
                 "cantrips": ["Mage Hand", "Prestidigitation", "Ray of Frost"],
                 "1st_level_spells": ["Magic Missile", "Shield", "Cure Wounds"],
             },
         )
         self.assertEqual(
-            alice.background,
+            instance.background,
             "Alice grew up in a small village where she was fascinated by the natural world and the secrets of the arcane. She left her village to study magic at a nearby wizard academy, where she excelled in her studies and gained a reputation as a diligent and creative student. After graduating, she set out on a journey to explore the world and use her magic to help others. Alice is driven by a desire to uncover new knowledge and",
         )
+
+    def from_dict(self, d):
+        return D20CharacterSheet.from_dict(d)
+    def test_init(self):
+        self.instance_test(self.instance)
 
     def test_to_dict(self):
-        # Create a D20CharacterSheet object
-        alice = D20CharacterSheet(
-            name="Alice",
-            race="Human",
-            char_class="Wizard",
-            level=3,
-            alignment="Neutral Good",
-            ability_scores={
-                "strength": 8,
-                "dexterity": 14,
-                "constitution": 12,
-                "intelligence": 16,
-                "wisdom": 10,
-                "charisma": 10,
-            },
-            skills={
-                "arcana": 6,
-                "history": 6,
-                "investigation": 6,
-                "nature": 6,
-                "religion": 6,
-            },
-            feats=[
-                {
-                    "name": "Alert",
-                    "description": "+5 initiative and cannot be surprised",
-                },
-                {
-                    "name": "Magic Initiate",
-                    "description": "Can cast two cantrips and one 1st-level spell from a chosen spellcasting class (cleric)",
-                },
-            ],
-            equipment=[
-                "Spellbook",
-                "Wand",
-                "Backpack",
-                "Bedroll",
-                "Rations (5 days)",
-                "Waterskin",
-                "20 gold pieces",
-            ],
-            spells_known={
-                "cantrips": ["Mage Hand", "Prestidigitation", "Ray of Frost"],
-                "1st_level_spells": ["Magic Missile", "Shield", "Cure Wounds"],
-            },
-            background="Alice grew up in a small village where she was fascinated by the natural world and the secrets of the arcane. She left her village to study magic at a nearby wizard academy, where she excelled in her studies and gained a reputation as a diligent and creative student. After graduating, she set out on a journey to explore the world and use her magic to help others. Alice is driven by a desire to uncover new knowledge and",
-        )
-
-        # Convert the object to a dictionary
-        alice_dict = alice.to_dict()
-
-        # Check that the dictionary has the correct values
-        self.assertEqual(alice_dict["name"], "Alice")
-        self.assertEqual(alice_dict["race"], "Human")
-        self.assertEqual(alice_dict["class"], "Wizard")
-        self.assertEqual(alice_dict["level"], 3)
-        self.assertEqual(alice_dict["alignment"], "Neutral Good")
-        self.assertEqual(
-            alice_dict["ability_scores"],
-            {
-                "strength": 8,
-                "dexterity": 14,
-                "constitution": 12,
-                "intelligence": 16,
-                "wisdom": 10,
-                "charisma": 10,
-            },
-        )
-        self.assertEqual(
-            alice_dict["skills"],
-            {"arcana": 6, "history": 6, "investigation": 6, "nature": 6, "religion": 6},
-        )
-        self.assertEqual(
-            alice_dict["feats"][0],
-            {"name": "Alert", "description": "+5 initiative and cannot be surprised"},
-        )
-        self.assertEqual(
-            alice_dict["feats"][1],
-            {
-                "name": "Magic Initiate",
-                "description": "Can cast two cantrips and one 1st-level spell from a chosen spellcasting class (cleric)",
-            },
-        )
-        self.assertEqual(
-            alice_dict["equipment"],
-            [
-                "Spellbook",
-                "Wand",
-                "Backpack",
-                "Bedroll",
-                "Rations (5 days)",
-                "Waterskin",
-                "20 gold pieces",
-            ],
-        )
-        self.assertEqual(
-            alice_dict["spells_known"],
-            {
-                "cantrips": ["Mage Hand", "Prestidigitation", "Ray of Frost"],
-                "1st_level_spells": ["Magic Missile", "Shield", "Cure Wounds"],
-            },
-        )
-        self.assertEqual(
-            alice_dict["background"],
-            "Alice grew up in a small village where she was fascinated by the natural world and the secrets of the arcane. She left her village to study magic at a nearby wizard academy, where she excelled in her studies and gained a reputation as a diligent and creative student. After graduating, she set out on a journey to explore the world and use her magic to help others. Alice is driven by a desire to uncover new knowledge and",
-        )
+        expected = self.instance_dict
+        actual = self.instance.to_dict()
+        self.assertEqual(expected, actual)
 
     def test_from_dict(self):
-        # Create a dictionary representing a D20CharacterSheet
-        alice_dict = {
-            "name": "Alice",
-            "race": "Human",
-            "class": "Wizard",
-            "level": 3,
-            "alignment": "Neutral Good",
-            "ability_scores": {
-                "strength": 8,
-                "dexterity": 14,
-                "constitution": 12,
-                "intelligence": 16,
-                "wisdom": 10,
-                "charisma": 10,
-            },
-            "skills": {
-                "arcana": 6,
-                "history": 6,
-                "investigation": 6,
-                "nature": 6,
-                "religion": 6,
-            },
-            "feats": [
-                {
-                    "name": "Alert",
-                    "description": "+5 initiative and cannot be surprised",
-                },
-                {
-                    "name": "Magic Initiate",
-                    "description": "Can cast two cantrips and one 1st-level spell from a chosen spellcasting class (cleric)",
-                },
-            ],
-            "equipment": [
-                "Spellbook",
-                "Wand",
-                "Backpack",
-                "Bedroll",
-                "Rations (5 days)",
-                "Waterskin",
-                "20 gold pieces",
-            ],
-            "spells_known": {
-                "cantrips": ["Mage Hand", "Prestidigitation", "Ray of Frost"],
-                "1st_level_spells": ["Magic Missile", "Shield", "Cure Wounds"],
-            },
-            "background": "Alice grew up in a small village where she was fascinated by the natural world and the secrets of the arcane. She left her village to study magic at a nearby wizard academy, where she excelled in her studies and gained a reputation as a diligent and creative student. After graduating, she set out on a journey to explore the world and use her magic to help others. Alice is driven by a desire to uncover new knowledge and",
-        }
+        # verify that we can change from dict to Character and back
+        instance_from_dict = self.from_dict(self.instance_dict)
+        self.instance_test(instance_from_dict)
 
-        # Create a D20CharacterSheet object from the dictionary
-        alice = D20CharacterSheet.from_dict(alice_dict)
-
-        # Check that the object has the correct values
-        self.assertEqual(alice.name, "Alice")
-        self.assertEqual(alice.race, "Human")
-        self.assertEqual(alice.char_class, "Wizard")
-        self.assertEqual(alice.level, 3)
-        self.assertEqual(alice.alignment, "Neutral Good")
-        self.assertEqual(
-            alice.ability_scores,
-            {
-                "strength": 8,
-                "dexterity": 14,
-                "constitution": 12,
-                "intelligence": 16,
-                "wisdom": 10,
-                "charisma": 10,
-            },
-        )
-        self.assertEqual(
-            alice.skills,
-            {"arcana": 6, "history": 6, "investigation": 6, "nature": 6, "religion": 6},
-        )
-        self.assertEqual(len(alice.feats), 2)
-        self.assertEqual(
-            alice.feats[0],
-            {"name": "Alert", "description": "+5 initiative and cannot be surprised"},
-        )
-        self.assertEqual(
-            alice.feats[1],
-            {
-                "name": "Magic Initiate",
-                "description": "Can cast two cantrips and one 1st-level spell from a chosen spellcasting class (cleric)",
-            },
-        )
-        self.assertEqual(
-            alice.equipment,
-            [
-                "Spellbook",
-                "Wand",
-                "Backpack",
-                "Bedroll",
-                "Rations (5 days)",
-                "Waterskin",
-                "20 gold pieces",
-            ],
-        )
-        self.assertEqual(
-            alice.spells_known,
-            {
-                "cantrips": ["Mage Hand", "Prestidigitation", "Ray of Frost"],
-                "1st_level_spells": ["Magic Missile", "Shield", "Cure Wounds"],
-            },
-        )
-        self.assertEqual(
-            alice.background,
-            "Alice grew up in a small village where she was fascinated by the natural world and the secrets of the arcane. She left her village to study magic at a nearby wizard academy, where she excelled in her studies and gained a reputation as a diligent and creative student. After graduating, she set out on a journey to explore the world and use her magic to help others. Alice is driven by a desire to uncover new knowledge and",
-        )
+        expected = self.instance_dict
+        actual = instance_from_dict.to_dict()
+        self.assertEqual(expected, actual)
 
 
 if __name__ == "__main__":
